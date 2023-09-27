8c807cb5aba552cc757adbfd1b2c340a8b62586e
Aurelio Borges <aurelioaborges@gmail.com>
2023-09-27 19:21:41 -0300

    CW-08: create Home Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
+import Error from '@/error/Error'
 import styles from './page.module.scss'
+import FilterNav from '@/components/filterNav/FilterNav'
 
 export default function Home() {
 	return (
-		<main className={styles.main}>
-			<div className={styles.description}>
+		<main className={styles['main']}>
+			<div className={styles['main__header']}>
+				<h1>Star Wars Characters</h1>
 				<p>
-					CloudWalk&nbsp;
-					<code className={styles.code}>Frontend Test</code>
+					Embark on an unforgettable journey into a galaxy far, far away!
+					Explore the rich tapestry of characters that populate this
+					extraordinary universe and unleash the power of the Force and unravel
+					the secrets of your favorite characters. May the Force be with you!
 				</p>
 			</div>
+			<FilterNav />
+			<Error />
 		</main>
 	)
 }
