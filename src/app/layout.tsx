58c4ebd6239fdf1d80dbdc8e6bdb2acd90eb3ae0
Aurelio Borges <aurelioaborges@gmail.com>
2023-09-27 19:14:58 -0300

    CW-02 config: setup React Icons and Next-Themes Libraries

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import './globals.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/footer/Footer'
+import Nav from '@/components/navBar/NavBar'
+import Providers from '@/utils/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-	title: 'Star Wars Finder',
+	title: 'Star Wars Characters',
 	description:
 		"All the Star Wars data you've ever wanted: Planets, Spaceships, Vehicles, People, Films and Species, From all SEVEN Star Wars films, Now with The Force Awakens data!"
 }
@@ -17,7 +20,13 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<Providers>
+					<Nav />
+					{children}
+					<Footer />
+				</Providers>
+			</body>
 		</html>
 	)
 }
---
+++ src/utils/Providers.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { ThemeProvider } from 'next-themes'
+import { useEffect, useState } from 'react'
+import Fallback from '@/fallback/Fallback'
+
+export default function Providers({ children }: any) {
+	const [mounted, setMounted] = useState(false)
+
+	useEffect(() => {
+		setMounted(true)
+	}, [])
+
+	if (!mounted) {
+		return <Fallback />
+	}
+
+	return <ThemeProvider>{children}</ThemeProvider>
+}
