9329cc9a4c79188476e6c5f14e3f50ff1ceeb804
Aurelio Borges <aurelioaborges@gmail.com>
2023-09-29 10:14:19 -0300

    CW-04 fix: missing id property to fix a console issue

--- src/components/filterNav/FilterNav.tsx
+++ src/components/filterNav/FilterNav.tsx
@@ -6,6 +6,7 @@ export default function FilterNav() {
 			<div className={styles['filter-nav__search']}>
 				<p>Filter by:</p>
 				<input
+					id='filterBy'
 					type='text'
 					placeholder='All'
 				/>
