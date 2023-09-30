5a9d6e756c4768f07aa7bd190bc8feccac145b5b
Aurelio Borges <aurelioaborges@gmail.com>
2023-09-30 15:13:13 -0300

    CW-10: implement real-time filtering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,17 @@
 
 import { useEffect, useState } from 'react'
 import { PeopleProps } from '@/types/characters'
-import Error from '@/error/Error'
 import styles from './page.module.scss'
+import Fallback from '@/fallback/Fallback'
 import FilterNav from '@/components/filterNav/FilterNav'
-import Image from 'next/image'
-import React from 'react'
+import Characters from '@/components/characters/Characters'
+import ActionButton from '@/components/actionButton/ActionButton'
 
 export default function Home() {
 	const [characters, setCharacters] = useState<PeopleProps | null>(null)
-	const [displayRows, setDisplayRows] = useState(2)
 	const [error, setError] = useState(false)
+	const [selectedPlanet, setSelectedPlanet] = useState<string>('')
+	const [displayRows, setDisplayRows] = useState(2)
 
 	const loadCharacters = async () => {
 		setCharacters(null)
@@ -50,6 +51,25 @@ export default function Home() {
 		loadCharacters()
 	}, [])
 
+	const filteredCharacters = characters
+		? characters.results.filter((character) => {
+				if (!selectedPlanet) {
+					return true
+				}
+				return (
+					character.homeworld.toLowerCase() === selectedPlanet.toLowerCase()
+				)
+		  })
+		: []
+
+	const homeworlds = characters
+		? Array.from(new Set(characters.results.map((char) => char.homeworld)))
+		: []
+
+	const clearFilters = () => {
+		setSelectedPlanet('')
+	}
+
 	const loadMore = () => {
 		setDisplayRows(displayRows + 1)
 	}
@@ -66,61 +86,31 @@ export default function Home() {
 				</p>
 			</div>
 
-			<FilterNav />
+			<FilterNav
+				selectedPlanet={selectedPlanet}
+				setSelectedPlanet={setSelectedPlanet}
+				clearFilters={clearFilters}
+				homeworlds={homeworlds}
+			/>
 
 			<h2 className={styles['char__heading']}>All Characters</h2>
 
-			<section className={styles['char__section']}>
-				{error && <Error />}
-
-				{characters &&
-					characters.results.slice(0, displayRows * 4).map((char, index) => {
-						const randomWidth = Math.floor(Math.random() * 500) + 600
-						const randomHeight = Math.floor(Math.random() * 500) + 320
-						const randomImageUrl = `https://picsum.photos/${randomWidth}/${randomHeight}`
-
-						return (
-							<div
-								className={styles['char__section--content']}
-								key={index}
-							>
-								<Image
-									className={styles['char__section--content-img']}
-									src={randomImageUrl}
-									alt={'Random image'}
-									width={432}
-									height={230}
-								/>
-
-								<div className={styles['char__section--content-info']}>
-									<p className={styles['name']}>{char.name}</p>
-									<p className={styles['homeworld']}>{char.homeworld}</p>
-								</div>
-
-								<div className={styles['char__section--content-stats']}>
-									<p>Height • {char.height}</p>
-									<p>Mass • {char.mass}</p>
-									<p>Gender • {char.gender}</p>
-								</div>
-							</div>
-						)
-					})}
-			</section>
-
-			{characters?.results && displayRows * 4 <= characters.results.length ? (
-				<button
-					className={styles['action-button']}
-					onClick={loadMore}
-				>
-					Load More
-				</button>
+			{!characters || !characters.results ? (
+				<Fallback />
 			) : (
-				<button
-					className={styles['action-button']}
-					onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-				>
-					Back to Top
-				</button>
+				<>
+					<Characters
+						error={error}
+						filteredCharacters={filteredCharacters}
+						displayRows={displayRows}
+					/>
+
+					<ActionButton
+						characters={characters}
+						displayRows={displayRows}
+						loadMore={loadMore}
+					/>
+				</>
 			)}
 		</main>
 	)
