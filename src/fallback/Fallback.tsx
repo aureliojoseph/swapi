7e58079093da2f97a3a26e148cc086059bcab902
Aurelio Borges <aurelioaborges@gmail.com>
2023-09-30 15:07:53 -0300

    CW-07 update: Fallback util component

--- src/fallback/Fallback.tsx
+++ src/fallback/Fallback.tsx
@@ -3,7 +3,7 @@ import styles from './fallback.module.scss'
 export default function Falback() {
 	return (
 		<div className={styles['fallback']}>
-			<h3>Loading...</h3>
+			<h3>The Force is Awakening!</h3>
 		</div>
 	)
 }
