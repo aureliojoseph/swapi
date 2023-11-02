d744dc7994926fd17f2d6491eeb2e2fb58091513
Aurelio Borges <aurelioaborges@gmail.com>
2023-11-02 10:43:55 -0300

    CW-03 fix: extract logic to conditionally render nav icons

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -7,19 +7,23 @@ import { useTheme } from 'next-themes'
 export default function NavBar() {
 	const { theme, setTheme } = useTheme()
 
-	return (
-		<nav className={styles['navbar']}>
-			{theme === 'dark' ? (
+	const renderNavIcons = () => {
+		if (theme === 'dark') {
+			return (
 				<PiSunBold
 					className={styles['navbar__icon']}
 					onClick={() => setTheme('light')}
 				/>
-			) : (
-				<PiMoonStarsBold
-					className={styles['navbar__icon']}
-					onClick={() => setTheme('dark')}
-				/>
-			)}
-		</nav>
-	)
+			)
+		}
+
+		return (
+			<PiMoonStarsBold
+				className={styles['navbar__icon']}
+				onClick={() => setTheme('dark')}
+			/>
+		)
+	}
+
+	return <nav className={styles['navbar']}>{renderNavIcons()}</nav>
 }
